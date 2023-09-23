f5b6fc165dbaa325cf8b079145ead07e69b8677f
SheerMr <107499829+SheerMr@users.noreply.github.com>
2023-09-23 22:08:06 +0200

    Update arrow.py

--- arrow.py
+++ arrow.py
@@ -1,6 +1,13 @@
 import tkinter
+from math import atan2, sqrt, sin, cos
 
-class Arrow:
+def draw_arrow(startx, endx, starty, endy):
+    theta = atan2(endx, endy)
+    if euclid_dist(startx, endx, starty, endy) < 30:
+        return
+    else:
+        rect = [sin(theta)*(startx + 5)
+            
 
-    def __init__(self, startx, endx, starty, endy) -> None:
-        
+def euclid_dist(x1, x2, y1, y2):
+    return sqrt((x1-x2)**2+(y1-y2)**2)
