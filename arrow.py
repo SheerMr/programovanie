dbcc1cd77cfefd0333158422a06dce72ef568f47
Rado Carsky <carsky.r@gjh.sk>
2023-09-24 15:28:26 +0200

    logika grafiky

--- arrow.py
+++ arrow.py
@@ -1,13 +1,55 @@
+from math import atan2, sqrt, sin, cos, pi
 import tkinter
-from math import atan2, sqrt, sin, cos
 
-def draw_arrow(startx, endx, starty, endy):
-    theta = atan2(endx, endy)
-    if euclid_dist(startx, endx, starty, endy) < 30:
-        return
-    else:
-        rect = [sin(theta)*(startx + 5)
-            
 
 def euclid_dist(x1, x2, y1, y2):
     return sqrt((x1-x2)**2+(y1-y2)**2)
+
+
+def draw_arrow(startx, starty, endx, endy):
+    theta = atan2(endx-startx, endy-starty)
+    dist = euclid_dist(startx, endx, starty, endy)
+    if dist < 30:
+        return (False, False)
+    else:
+        rect = [startx+5*cos(pi/2-theta-pi/4), starty+5*sin(pi/2-theta-pi/4),
+                startx+5*cos(pi/2-theta+pi/4), starty+5*sin(pi/2-theta+pi/4),
+                startx+cos(pi/2-theta+pi/(4*((dist-30)/5)))*(dist-30),
+                starty+sin(pi/2-theta+pi/(4*((dist-30)/5)))*(dist-30),
+                startx+cos(pi/2-theta-pi/(4*((dist-30)/5)))*(dist-30),
+                starty+sin(pi/2-theta-pi/(4*((dist-30)/5)))*(dist-30),
+                ]
+        triangle = [endx, endy,
+                startx+cos(pi/2-theta+2*pi/(4*((dist-30)/5)))*(dist-30),
+                starty+sin(pi/2-theta+2*pi/(4*((dist-30)/5)))*(dist-30),
+                startx+cos(pi/2-theta-2*pi/(4*((dist-30)/5)))*(dist-30),
+                starty+sin(pi/2-theta-2*pi/(4*((dist-30)/5)))*(dist-30)]
+
+    return rect, triangle
+
+
+if __name__ == '__main__':
+    canvas = tkinter.Canvas(width=500, height=500)
+    canvas.pack()
+    angle = 0
+    length = 70
+    d_len = 10
+    arrow = None
+
+    def toc_sa():
+        global angle, canvas, length, d_len, arrow
+        if angle >= 2*pi:
+            angle = 0
+        if not(70<=length<=300):
+            d_len *= -1
+        canvas.delete('all')
+        rect, triangle = draw_arrow(250, 250, 250+length*cos(angle), 250+length*sin(angle))
+        canvas.create_polygon(*rect)
+        canvas.create_polygon(*triangle)
+        
+        angle += 0.05
+        length += d_len
+        canvas.after(50, toc_sa)
+
+    toc_sa()
+    canvas.mainloop()
--- board.py
+++ board.py
@@ -1,9 +1,12 @@
 import tkinter
 from time import sleep
+from arrow import draw_arrow
+from math import atan2, sin, cos
+
 
 class Chessboard:
 
-    TRAVELTIME = 1
+    TRAVELTIME = 10
     DIST = 5 # arbitrary units
     FREQ = 50
 
@@ -23,13 +26,26 @@ class Chessboard:
     def move_piece(self, piece, x , y):
         if piece.pointer:
             self.canvas.delete(piece.pointer)
+        
+        startx = piece.x*self.TILESIZE
+        starty = piece.y*self.TILESIZE
+        angle = atan2(x*self.TILESIZE-startx, y*self.TILESIZE-starty)
             
         x_speed = ((x-piece.x)*self.TILESIZE)/(Chessboard.FREQ*Chessboard.TRAVELTIME)
         y_speed = ((y-piece.y)*self.TILESIZE)/(Chessboard.FREQ*Chessboard.TRAVELTIME)
-        for x in range(Chessboard.TRAVELTIME*Chessboard.FREQ):
+        for i in range(Chessboard.TRAVELTIME*Chessboard.FREQ):
             self.canvas.move(piece.sprite, x_speed, y_speed)
+            rect, arrow = draw_arrow(startx+self.TILESIZE/2, starty+self.TILESIZE/2,
+                                      startx+(i+1)*x_speed+self.TILESIZE/2, starty+(i+1)*y_speed+self.TILESIZE/2)
+            if rect and arrow:
+                piece.pointer.append(self.canvas.create_polygon(*rect))
+                piece.pointer.append(self.canvas.create_polygon(*arrow))
             board.canvas.update()
+            while piece.pointer:
+                self.canvas.delete(piece.pointer.pop())
             sleep(1/Chessboard.FREQ)
+        piece.x = x
+        piece.y = y
             
 
     def mainloop(self):
@@ -47,11 +63,11 @@ class Piece:
     def __init__(self, x, y, board) -> None:
         self.x = x
         self.y = y
-        self.pointer = None
+        self.pointer = []
         self.board = board
         self.radius = board.TILESIZE/2 * 0.8
         self.sprite = board.canvas.create_oval((x+0.5)*board.TILESIZE-self.radius, (y+0.5)*board.TILESIZE-self.radius,
-                                                     (x+0.5)*board.TILESIZE+self.radius, (y+0.5)*board.TILESIZE+self.radius, fill='blue')
+                        (x+0.5)*board.TILESIZE+self.radius, (y+0.5)*board.TILESIZE+self.radius, fill='blue')
 
 
 if __name__ == "__main__":
